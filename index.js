2b4ede8b0cdb49fae10a8e86adde6ac3e24dc0ad
zhangguangpu <1136062041@qq.com>
2020-08-23 11:43:43 +0800

    基础结构

---
+++ index.js
@@ -0,0 +1,56 @@
+var Async = function (fn) {
+    this.name = 'Async'
+    this._state = 'pendding'
+    if (typeof fn !== 'function') {
+        throw new Error('The Param Of Async Must Be Typeof Function !')
+        return null
+    }
+    var resolve = (function (data) {
+        this._state = 'fulfilled'
+        typeof this._resolve === 'function' && this._resolve(data) 
+        return this
+    }).bind(this)
+    var reject = (function (err) {
+        this._state = 'reject'
+        typeof this._catch === 'function' ? this._catch(err) : (typeof this._reject === 'function' && this._reject(data) && this._reject(err))
+        return this
+    }).bind(this)
+    fn.call(this, resolve, reject)
+    typeof this._finally === 'function' && this._finally() 
+}
+Async.prototype.then = function (resolve, reject) {
+    this._resolve = resolve
+    this._reject = reject
+}
+Async.prototype.catch = function (reject) {
+    this._catch = reject
+}
+Async.prototype.finally = function (cb) {
+    this._finally = cb
+}
+Async.resolve = function (any) {
+    if(any instanceof Async) return any
+    return new Async(function(resolve) {
+        resolve(any)
+    })
+}
+Async.reject = function (any) {
+    if(any instanceof Async) return any
+    return new Async(function(resolve, reject) {
+        reject(any)
+    })
+}
+Async.all = function () {
+
+}
+Async.race = function () {
+    
+}
+Async.allSettled = function () {
+    
+}
+Async.any = function () {
+    
+}
+
+module.exports = Async
--- test/base.js
+++ test/base.js
@@ -1,12 +1,3 @@
-# async
-对ES6 Promise 的原生实现，可用于不支持Promise的浏览器。
----
-## install
-npm install -g normal-async
-## github
-https://github.com/rookie-mr/async.git
-### 基本用法
-```javascript
 var async = new Async(function (resolve, reject) {
     setTimeout(function () {
         resolve(9527)
@@ -20,7 +11,3 @@ async.then(function (data) {
     console.log('reject')
 })
 console.log(async)
-
-Async.resolve()
-Async.reject()
-```
